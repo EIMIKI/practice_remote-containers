4388ab529513ca8a7733ad46aae875f4c18a82d6
EIMIKI <kou.103.kan@gmail.com>
2020-07-02 12:08:56 +0000

    テーブル定義変更

--- create_table.py
+++ create_table.py
@@ -1,42 +1,34 @@
 import boto3
 import os
 
-def create_movie_table(dynamodb=None):
+def create_log_table(dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource(
             'dynamodb', endpoint_url="http://dynamo:8000")
 
     table = dynamodb.create_table(
-        TableName='Movies',
+        TableName='Logs',
         KeySchema=[
             {
-                'AttributeName': 'year',
+                'AttributeName': 'time',
                 'KeyType': 'HASH'  # Partition key
-            },
-            {
-                'AttributeName': 'title',
-                'KeyType': 'RANGE'  # Sort key
             }
         ],
         AttributeDefinitions=[
             {
-                'AttributeName': 'year',
-                'AttributeType': 'N'
-            },
-            {
-                'AttributeName': 'title',
+                'AttributeName': 'time',
                 'AttributeType': 'S'
-            },
+            }
 
         ],
         ProvisionedThroughput={
-            'ReadCapacityUnits': 10,
-            'WriteCapacityUnits': 10
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
         }
     )
     return table
 
 
 if __name__ == '__main__':
-    movie_table = create_movie_table()
-    print("Table status:", movie_table.table_status)
+    log_table = create_log_table()
+    print("Table status:", log_table.table_status)
