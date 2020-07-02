4e0879980c0377c4f753c3d7441dc8340088519d
EIMIKI <kou.103.kan@gmail.com>
2020-07-02 14:32:59 +0000

    アクセスのたびにデータの増える謎app

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask
-from redis import Redis
+from flask import Flask, render_template
+import datetime
+import boto3
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+dynamodb = boto3.resource('dynamodb', endpoint_url="http://dynamo:8000")
+
+
+def put_time():
+
+    table = dynamodb.Table('Logs')
+    response = table.put_item(
+        Item={
+            'time': str(datetime.datetime.now())
+        }
+    )
+    return response
+
+
+def scan_times():
+    table = dynamodb.Table('Logs')
+    response = table.scan()
+
+    return response['Items']
+
 
 @app.route('/')
 def hello():
-    count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    put_time()
+    times = scan_times()
+    return render_template('index.html', times=times)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
